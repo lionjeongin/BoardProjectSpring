74d196f3b220b2190be9668833be56f4b2e0dcdb
lionjeongin <cindy93489163@gmail.com>
2024-07-29 15:43:42 +0900

    회원가입 검증

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,21 +1,35 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.choongang.member.validators.JoinValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final JoinValidator joinValidator;
+
     @GetMapping("/join")
-    public String join() {
+    public String join(@ModelAttribute RequestJoin form) {
         return "front/member/join";
     }
 
     @PostMapping("/join")
-    public String joinPs() {
+    public String joinPs(@Valid RequestJoin form, Errors errors) {
+
+        joinValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            return "front/member/join";
+        }
 
         return "redirect:/member/login";
     }
