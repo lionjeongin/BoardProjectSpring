43d63582c2a82ccf152ba418e071a8046a41effc
lionjeongin <cindy93489163@gmail.com>
2024-07-31 17:28:38 +0900

    스프링 시큐리티

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -2,13 +2,24 @@ package org.choongang.member.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.choongang.board.entities.Board;
+import org.choongang.board.repositories.BoardRepository;
+import org.choongang.member.MemberInfo;
+import org.choongang.member.MemberUtil;
 import org.choongang.member.services.MemberSaveService;
 import org.choongang.member.validators.JoinValidator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
+@Slf4j
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -17,6 +28,8 @@ public class MemberController {
 
     private final JoinValidator joinValidator;
     private final MemberSaveService memberSaveService;
+    private final MemberUtil memberUtil;
+    private final BoardRepository boardRepository;
 
     @ModelAttribute
     public RequestLogin requestLogin() {
@@ -48,11 +61,64 @@ public class MemberController {
         if (StringUtils.hasText(code)) {
             errors.reject(code, form.getDefaultMessage());
 
+            // 비번 만료인 경우 비번 재설정 페이지 이동
             if (code.equals("CredentialsExpired.Login")) {
                 return "redirect:/member/password/reset";
             }
         }
 
         return "front/member/login";
+    }
+
+    @ResponseBody
+    @GetMapping("/test")
+    public void test(Principal principal) {
+        log.info("로그인 아이디: {}", principal.getName());
+    }
+
+    @ResponseBody
+    @GetMapping("/test2")
+    public void test2(@AuthenticationPrincipal MemberInfo memberInfo) {
+        log.info("로그인 회원: {}", memberInfo.toString());
+    }
+
+    @ResponseBody
+    @GetMapping("/test3")
+    public void test3() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+
+
+        log.info("로그인 상태: {}", authentication.isAuthenticated());
+        if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof MemberInfo) { // 로그인 상태 - UserDetails 구현체(getPrincipal())
+            MemberInfo memberInfo = (MemberInfo) authentication.getPrincipal();
+            log.info("로그인 회원: {}", memberInfo.toString());
+        } else { // 미로그인 상태 - String / anonymousUser (getPrincipal())
+            log.info("getPrincipal(): {}", authentication.getPrincipal());
+        }
+    }
+
+    @ResponseBody
+    @GetMapping("/test4")
+    public void test4() {
+        log.info("로그인 여부: {}", memberUtil.isLogin());
+        log.info("로그인 회원: {}", memberUtil.getMember());
+    }
+
+    @ResponseBody
+    @GetMapping("/test5")
+    public void test5() {
+        /*
+        Board board = Board.builder()
+                .bId("freetalk")
+                .bName("자유게시판")
+                .build();
+
+        boardRepository.saveAndFlush(board);
+
+         */
+        Board board = boardRepository.findById("freetalk").orElse(null);
+        board.setBName("(수정)자유게시판");
+        boardRepository.saveAndFlush(board);
     }
 }
