53414c0f91f3da096242d8129a9f203e8e039662
lionjeongin <cindy93489163@gmail.com>
2024-07-31 11:27:15 +0900

    로그인 예외 발생 처리 : 탈퇴한 회원, 비밀번호 유효만료, 아이디와 비밀번호 일치하지 않을 때, 사용자 계정 유효 기간

--- src/main/java/org/choongang/global/configs/SecurityConfig.java
+++ src/main/java/org/choongang/global/configs/SecurityConfig.java
@@ -1,17 +1,36 @@
 package org.choongang.global.configs;
 
+import org.choongang.member.services.LoginFailureHandler;
+import org.choongang.member.services.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /* 로그인, 로그아웃 S */
+        http.formLogin(f -> {
+            f.loginPage("/member/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .successHandler(new LoginSuccessHandler())
+                    .failureHandler(new LoginFailureHandler());
+        });
+
+
+        http.logout(f -> {
+            f.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                    .logoutSuccessUrl("/member/login");
+
+        });
+        /* 로그인, 로그아웃 E */
 
         return http.build();
     }
---
+++ src/main/java/org/choongang/member/controllers/RequestLogin.java
@@ -0,0 +1,18 @@
+package org.choongang.member.controllers;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RequestLogin {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private boolean success = true;
+
+    private String code;
+    private String defaultMessage;
+}
